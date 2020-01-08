39e6042954c870a8e0ed74887862af38bbafc5b2
Lucas Mendes Loureiro <mendes_lucas2006@hotmail.com>
2020-01-08 00:29:28 -0300

    feat: implementing routing decoratros

---
+++ src/decorators.js
@@ -0,0 +1,120 @@
+const {
+	isString,
+	isFunction
+}  =  require('util')
+
+const serverMethods   = [ 'get', 'post', 'put', 'patch', 'delete', 'head', 'options', 'all' ]
+
+const extractDecoratorParams = args => {
+	let [ argValue,  ...middlewares ] = args
+	let mainPath = ''
+
+	if (isString(argValue)) mainPath = argValue
+
+	if (isFunction(argValue)) middlewares = [argValue, ...middlewares]
+
+	if (argValue && mainPath === '' && middlewares.length === 0) throw new TypeError('The first argument is not a valid string path or middleware function')
+
+	if (!middlewares.every(isFunction)) throw new TypeError('invalid type, middleware must be a function')
+
+	return [ mainPath, middlewares ]
+}
+
+const createMetadataMapper = (rootPath, controllerMiddlewares) => metadataItem => {
+	const [ endpointFn, metadata ] = metadataItem
+
+	const {
+		path,
+		httpMethod,
+		middlewares
+	} = metadata
+
+	const actionPath = `${rootPath}${path || ''}`
+	const actionMiddlewares = controllerMiddlewares.concat(middlewares)
+
+	return {
+		httpMethod,
+		endpointFn,
+		actionPath,
+		actionMiddlewares
+	}
+}
+
+/**
+ * Responsible to create a valid express routing method, adding metadata routes to object or prototype.
+ * This method is used with controller decorator. 
+ * 
+ * @param  {String} method represents a valid express routing method
+ * @param  {Function} [args=[]] represents a middleware collection to pass into express router
+ * @returns {Function}  this method returns a function receiving the target object with a function name to decorate
+ */
+const route = (method, ...args) => {
+
+	if (!isString(method)) throw new TypeError('httpMethod must be a string')
+
+	if (!serverMethods.includes(method.toLowerCase())) throw new Error('method must be a valid express routing function')
+
+	const [ path, middlewares ] = extractDecoratorParams(args)
+
+	return function (target, property) {
+		if (!target[property])
+			throw new Error(`a property ${property} doesn't exists in target object`)
+
+		if (!target['$metadata'])
+			target.$metadata = new Map()
+
+		target.$metadata.set(property, { httpMethod: method, path, middlewares })
+	}
+}
+
+
+/**
+ * Responsible to define a object or object prototype as a controller.
+ * This method creates a high order function (HoF) to specify a route 
+ * metadata and apply into controller.
+ * 
+ * @param {String} controllerRootPath represents a controller root path
+ * @param {Function[]} [middlewares=[]] represents a middleware collection that will be applied in all methods in controller
+ * @returns {Function}  this method returns a function receiving the target object with a function name to decorate
+ * @example
+ * //prototype based
+ * class MyController {
+ *     constructor(server) { this.myDependency = server.myDependecy } 
+ *
+ *     get(req, res){ res.json(this.myDependency.myList) }
+ * }
+ *
+ * module.exports = controller('/my-controller', ...anyMiddlewareCollection)(MyController, {
+ *    get: route('get') //or use get()
+ * })
+ *
+ * //literal object based
+ * const getFunction = (req, res) => res.json({ message: 'my message' })
+ *
+ * module.exports = controller('/my-new-controller', ...anyMiddlewareCollection)({
+ *     getFunction
+ * }, {
+ *     getFunction: route('get') //or use get()
+ * })
+ */
+const controller = (controllerPath = '', ...middlewares) => { 
+	if (!isString(controllerPath)) throw new TypeError('controllerPath is not a valid string') 
+    
+	return function (target, handler) {
+		const proto     = target.prototype || target
+		Object.keys(handler)
+			.forEach(property => handler[property](proto, property))
+    
+		proto.$routes   = proto.$metadata ? 
+			[...proto.$metadata.entries()]
+				.map(createMetadataMapper(controllerPath || '', middlewares)) :
+			[]
+
+		return target
+	}
+}
+
+serverMethods.forEach(method => exports[method === 'delete' ? 'del' : method] = route.bind(null, method))
+
+exports.route = route
+exports.controller = controller
---
+++ test/decorators.spec.js
@@ -0,0 +1,165 @@
+/* eslint-disable no-undef */
+const chai = require('chai')
+const decorators = require('../src/decorators')
+
+const { expect } = chai
+chai.config.includeStack = true
+
+const noop = () => {}
+const noopMl = () => {}
+const noopMl2 = () => {}
+
+describe('express routing decorators', () => {
+	describe('controller and http methods decorators', () => {
+		class MyController { myMethod () { } }
+
+		it('should throw if controller receive non-string path', () => {
+			expect(() => {
+				decorators.controller(1)(class Ctrl {}, {})
+			}).to.throw('controllerPath is not a valid string')
+		})
+
+		it('should throw if action decorator receive non-string path', () => {
+			expect(() => {
+				decorators.controller()(MyController, {
+					myMethod: decorators.get(1)
+				})
+			}).to.throw('The first argument is not a valid string path or middleware function')
+		})
+
+		it('should add route metadata in controller object', () => {
+			const Controller = decorators.controller()(class Ctrl {}, {})
+
+			const instance = new Controller()
+			expect(instance).has.property('$routes')
+		})
+
+		it('should concat rootPath when action has a child path', () => {
+			const Controller = decorators.controller('/test-controller')(
+				MyController, {
+					myMethod: decorators.get('/:id')
+				}
+			)
+
+			const instance = new Controller()
+			expect(instance).to.have.property('$routes').with.lengthOf(1)
+			expect(instance.$routes).to.eql([{
+				httpMethod: 'get',
+				endpointFn: 'myMethod',
+				actionPath: '/test-controller/:id',
+				actionMiddlewares: []
+			}])
+		})
+
+		it('should throws error when action decorator middleware receive non-function parameter', () => {
+			expect(() => {
+				decorators.controller('/rootPath')({
+					myGetFn: noop
+				}, { myGetFn: decorators.post('/another', 1) })
+			}).to.throw('invalid type, middleware must be a function')
+		})
+
+		it('should normalize delete method', () => {
+			const Controller = decorators.controller('/my-controller')(
+				MyController, {
+					myMethod: decorators.del()
+				}
+			)
+
+			const instance = new Controller()
+			expect(instance).to.have.property('$routes').with.lengthOf(1)
+			expect(instance.$routes).to.eql([{
+				httpMethod: 'delete',
+				endpointFn: 'myMethod',
+				actionPath: '/my-controller',
+				actionMiddlewares: []
+			}])
+		})
+
+		it('should pass middleware function to controller decorator', () => {
+			const controller = decorators.controller('/literal-controller', noop)({
+				endpoint () {},
+				anotherEndpoint () {}
+			}, {
+				endpoint: decorators.put('/:id'),
+				anotherEndpoint: decorators.post('/post')
+			})
+
+			expect(controller).to.have.property('$routes').with.lengthOf(2)
+			expect(controller.$routes).to.eql([{
+				httpMethod: 'put',
+				endpointFn: 'endpoint',
+				actionPath: '/literal-controller/:id',
+				actionMiddlewares: [noop]
+			}, {
+				httpMethod: 'post',
+				endpointFn: 'anotherEndpoint',
+				actionPath: '/literal-controller/post',
+				actionMiddlewares: [noop]
+			}])
+		})
+
+		it('should throws error when route decorator receive a invalid express router method', () => {
+			expect(() => {
+				decorators.controller()(MyController, {
+					myMethod: decorators.route('test')
+				})
+			}).to.throw('method must be a valid express routing function')
+		})
+
+		it('should add metadata when controller have an action get', () => {
+			const Controller = decorators.controller('/my-controller')(MyController, {
+				myMethod: decorators.get()
+			})
+
+			const instance = new Controller()
+			expect(instance).to.have.property('$routes').with.lengthOf(1)
+			expect(instance.$routes).to.eql([{
+				httpMethod: 'get',
+				endpointFn: 'myMethod',
+				actionPath: '/my-controller',
+				actionMiddlewares: []
+			}])
+		})
+
+		it('should concat action middlewares with controller middlewares', () => {
+			const controller = decorators.controller('/my-controller', noop)({
+				endpoint () { }
+			}, {
+				endpoint: decorators.post(noopMl, noopMl2)
+			})
+
+			expect(controller).to.have.property('$routes').with.lengthOf(1)
+			expect(controller.$routes).to.eql([{
+				httpMethod: 'post',
+				endpointFn: 'endpoint',
+				actionPath: '/my-controller',
+				actionMiddlewares: [noop, noopMl, noopMl2]
+			}])
+		})
+
+		it('should throws error when try to pass unexpected property to decorator handler', () => {
+			expect(() => {
+				decorators.controller('/test')({}, {
+					anotherEndpoint: decorators.get()
+				})
+			}).to.throw('a property anotherEndpoint doesn\'t exists in target object')
+		})
+
+		it('should add metadata when controller have an action post', () => {
+			const controller = decorators.controller('/my-new-controller')({
+				myNewFunction () { }
+			}, {
+				myNewFunction: decorators.post()
+			})
+
+			expect(controller).to.have.property('$routes').with.lengthOf(1)
+			expect(controller.$routes).to.eql([{
+				httpMethod: 'post',
+				endpointFn: 'myNewFunction',
+				actionPath: '/my-new-controller',
+				actionMiddlewares: []
+			}])
+		})
+	})
+})
